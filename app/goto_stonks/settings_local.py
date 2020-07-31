8b4125561559432538ba608bf63a21c67ae55592
Kirill Abakunov <abakunovkirill@outlook.com>
2020-08-01 00:15:23 +0300

    Errors fixed

---
+++ app/goto_stonks/settings_local.py
@@ -0,0 +1,18 @@
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+import os
+import os.path
+import sys
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+SECRET_KEY = 'hdhdh'
+ALLOWED_HOSTS = ['']
+DEBUG = True
