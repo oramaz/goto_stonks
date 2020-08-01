ff8aced7f8efb0ff8fe50537df4549def9f37cd1
moitimemasters <ivanpesnia04@gmail.com>
2020-08-01 13:43:16 +0300

    json_to_kirill

--- bot/main.py
+++ bot/main.py
@@ -1,13 +1,13 @@
 import telebot
 import datetime
 import json
-from bot.model import News
 
 token = "914437279:AAFR6BIfw3B7JbdZaEnAMNL1boarkUnLMoc"
 
 bot = telebot.TeleBot(token)
 
-# news = []
+news = []
+
 
 @bot.message_handler(content_types=['text'])
 def get_news(message):
@@ -15,18 +15,20 @@ def get_news(message):
     if "#stonks" in message.text:
         news_content = message.text.replace("#stonks", "").strip()
         if news_content:
-            stamp = datetime.datetime.utcnow()
-            new = News(text=news_content, date=stamp)
-            new.save()
+            stamp = str(datetime.datetime.utcnow())
+            news += [{"content": news_content, "timestamp": stamp}]
+            update_news(news)
         # bot.send_message(message.from_user.id,
         #                  message.text.replace("#stonks", ""))
+
 
 def update_news(news):
     print(json.dumps(news))
     data = json.dumps(news, ensure_ascii=False).encode("utf-8")
-    print(data.decode("utf-8"))
-    with open("news.json", "w", encoding="utf-8") as f:
+    # print(data.decode("utf-8"))
+    with open("../app/news.json", "w", encoding="utf-8") as f:
         f.write(data.decode("utf-8"))
+
 
 @bot.message_handler(content_types=['document', 'audio', 'photo'])
 def get_photos(message):
@@ -34,12 +36,11 @@ def get_photos(message):
     if "#stonks" in message.caption:
         news_content = message.caption.replace("#stonks", "").strip()
         if news_content:
-            stamp = datetime.datetime.utcnow()
-            new = News(text=news_content, date=stamp)
-            new.save()
+            stamp = str(datetime.datetime.utcnow())
+            news += [{"content": news_content, "timestamp": stamp}]
+            update_news(news)
         # bot.send_message(message.from_user.id,
         #                  message.caption.replace("#stonks", ""))
 
 
 bot.polling(none_stop=True, interval=0)
-conn.close()
