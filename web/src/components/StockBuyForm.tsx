164ad351bde5f0c930d1390fb9797a531c43fa14
moitimemasters <ivanpesnia04@gmail.com>
2020-08-01 08:28:27 +0300

    form design

--- web/src/components/StockBuyForm.tsx
+++ web/src/components/StockBuyForm.tsx
@@ -1,25 +1,32 @@
 import React from "react";
-
+import "../assets/styles/Form.css";
 type Props = {
   onBuy: () => void;
 };
 const StockBuyForm = (props: Props) => {
   return (
     <div>
-      <form>
+      <form
+        onSubmit={() => {
+          props.onBuy();
+        }}
+      >
         <p>Купить акцию</p>
-        <input
-          type="text"
-          placeholder="Введите цену акции"
-          defaultValue="100"
-        ></input>
-        <span>готублей</span>
-        <input
-          type="text"
-          placeholder="Введите кол-во акций для продажи"
-          defaultValue="1"
-        ></input>
-        <span>акций</span>
+        <div className="form-content">
+          <input
+            type="text"
+            placeholder="Введите цену акции"
+            defaultValue="100"
+          ></input>
+          <span>готублей</span>
+          <input
+            type="text"
+            placeholder="Введите кол-во акций для продажи"
+            defaultValue="1"
+            id="stock"
+          ></input>
+          <span>акций</span>
+        </div>
         <br></br>
         <button type="submit">Submit</button>
       </form>
